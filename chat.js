9dda710483c68afd517a3bf7be00c732db492433
Raphael Charpy <raphael.charpy@etu.univ-amu.fr>
2019-04-30 17:59:59 +0200

    suite de la bd

--- chat.js
+++ chat.js
@@ -4,6 +4,24 @@ var ent = require('ent');
 var app = express();
 var server = require('http').createServer(app);
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "mysql-projetl3.alwaysdata.net",
+    user: "176620",
+    password: "projetjeudecarte",
+    database: "jeudecarte_bd"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT Mail, Password FROM User", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+}); // connexion
+
+
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public/views');
 
@@ -19,7 +37,21 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('newMessage', function (message) {
       socket.broadcast.emit('newMessage', "<p><b>" + socket.pseudo + "</b> : " + ent.encode(message) + "</p>");
-    })
+    });
+
+    socket.on('createAccount', function (username,email,password) {
+        con.connect(function(err) {
+            if (err) throw err;
+            console.log("Connected!");
+            var sql = "INSERT INTO User (Username, Mail, Password, Confirmed) VALUES(" +  username + "," + email +  "," + password + ")";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 user add");
+            });
+        }); // inscription
+
+        socket.broadcast.emit('createAccount', username,email,password);
+    });
 
     socket.on('nouvellePersonne', function(pseudo) {
       socket.pseudo = ent.encode(pseudo);
@@ -27,4 +59,4 @@ io.sockets.on('connection', function (socket) {
     });
 });
 
-server.listen(8100,"0.0.0.0"); // 8100,"0.0.0.0" - 8080
+server.listen(8080,"0.0.0.0"); // 8100,"0.0.0.0" - 8080
